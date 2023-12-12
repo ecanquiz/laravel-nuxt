bc7ae8eb00f7a7ed7a3fdf4aa3a23fb1907c4ae3
Ernesto Canquiz <caribestic123@gmail.com>
2023-12-12 11:55:19 -0400

    feat: Validar formulario del login

--- components/auth/FormLogin.vue
+++ components/auth/FormLogin.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
-  import { ref } from "vue"
+  import { reactive } from "vue"
   import type { FormLogin } from "types/auth";
+  import type { FormError, FormSubmitEvent } from '#ui/types'
 
   const props = defineProps<{
-    error?: object | string | null,
+    error?: object | string | undefined,
     sending: boolean
   }>()  
 
-  const email = ref('')  
-  const password = ref('')
+  const state = reactive({
+    email: '',
+    password : ''
+  })
+
+  const validateEmail = (email:string) => /\S+@\S+\.\S+/.test(email)
+  const validatePassword = (password:string) => password.length > 5
+
+  const validate = (state: any): FormError[] => {
+    const errors = []
+    if (!state.email) errors.push({ path: 'email', message: 'Required' })
+    if (!state.password) errors.push({ path: 'password', message: 'Required' })
+    if (!validateEmail(state.email)) errors.push({ path: 'email', message: 'Requires a valid email' })
+    if (!validatePassword(state.password)) errors.push({ path: 'password', message: 'The password must be at least 6 characters' })
+
+    return errors
+  }
 
   const emit = defineEmits<{
     (e: 'submit', { email, password }: FormLogin): void
@@ -16,33 +32,40 @@
   
   const submit = async () => {
     emit('submit', {
-      email: email.value,
-      password: password.value
+      email: state.email,
+      password: state.password
     })
   }
 </script>
 
 <template>
-  <form @submit.prevent="submit">
-    <UInput
+  <!--form @submit.prevent="submit"-->
+  <UForm :validate="validate" :state="state" class="space-y-4" @submit.prevent="submit">
+    <UFormGroup label="Email" name="email">
+      <UInput
       type="email"
       label="Correo Electrónico"
       name="email"
-      v-model="email"
+      v-model="state.email"
       autocomplete="email"
       placeholder="email@domain.ext"      
       class="mb-2"
       data-testid="email-input"      
     />
-    <UInput
-      type="password"
-      label="Contraseña"
-      name="password"
-      placeholder="password"
-      v-model="password"
-      class="mb-4"
-      data-testid="password-input"
-    />    
+    </UFormGroup>
+
+    <UFormGroup label="Password" name="password">
+      <UInput
+        type="password"
+        label="Contraseña"
+        name="password"
+        placeholder="password"
+        v-model="state.password"
+        class="mb-4"
+        data-testid="password-input"
+      />
+    </UFormGroup>
+   
 
     <div class="flex items-center justify-between mt-4">
       <label class="flex items-center">
@@ -60,6 +83,7 @@
         data-testid="submit-btn"
       >{{ props.sending ? 'Iniciando sesión...' : 'Iniciar sesión' }}</UButton>
     </div>
-    <!--AppFlashMessage :error='props.error' /-->
-  </form>
+    <AppFlashMessage :error='props.error' />
+  </UForm>
+  <!--/form-->
 </template>
